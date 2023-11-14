c89d6845fee3c4f7c3a8f577cda779fcedec19fc
MKLepium <maximilian.kurzawski@gmail.com>
2023-11-14 22:12:18 +0100

    only god knows how this works

--- src/wine/find_min_and_max_values_for_wine.py
+++ src/wine/find_min_and_max_values_for_wine.py
@@ -1,30 +1,52 @@
 import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
 
-
-def find_min_max_values(csv_file):
+def generate_samples_with_quality(csv_file, num_samples):
     df = pd.read_csv(csv_file, delimiter=';')
-    # Find the min and max for each column
-    min_values = df.min()
-    max_values = df.max()
-    return min_values, max_values
-
+    columns = df.columns
+    samples = {}
+    
+    for column in columns:
+        if column == 'quality':
+            # Set arbitrary quality values to 0 for all samples
+            samples[column] = np.zeros(num_samples)
+        else:
+            # Calculate the mean and standard deviation for each column (excluding 'quality')
+            mean = df[column].mean()
+            std = df[column].std()
+            
+            # Generate random samples based on a normal distribution with the calculated mean and std
+            samples[column] = np.random.normal(mean, std, num_samples)
+    
+    generated_df = pd.DataFrame(samples)
+    
+    # Exclude the 'quality' column for K-Means clustering
+    kmeans_columns = [col for col in columns if col != 'quality']
+    kmeans_data = generated_df[kmeans_columns]
+    
+    # Fit a K-Means model to the data without the 'quality' column
+    kmeans = KMeans(n_clusters=5, random_state=0).fit(kmeans_data)
+    
+    # Predict cluster labels for the generated data
+    generated_cluster_labels = kmeans.predict(kmeans_data)
+    
+    # Calculate the cluster centers for the generated data
+    cluster_centers = kmeans.cluster_centers_
+    
+    # Update the 'quality' column in the generated DataFrame based on the cluster centers
+    generated_df['quality'] = cluster_centers[generated_cluster_labels]
+    
+    return generated_df
 
 white_wine_path = 'wine+quality/winequality-white.csv'
 red_wine_path = 'wine+quality/winequality-red.csv'
 
-# Find the min and max values for white wine data
-white_wine_min, white_wine_max = find_min_max_values(white_wine_path)
-# Find the min and max values for red wine data
-red_wine_min, red_wine_max = find_min_max_values(red_wine_path)
+# Generate 100 samples for white wine data with arbitrary quality values
+white_wine_samples = generate_samples_with_quality(white_wine_path, num_samples=100)
+# Generate 100 samples for red wine data with arbitrary quality values
+red_wine_samples = generate_samples_with_quality(red_wine_path, num_samples=100)
 
-print("White wine min values: \n", white_wine_min)
+print("White wine generated samples with adjusted quality: \n", white_wine_samples)
 print()
-print("White wine max values: \n", white_wine_max)
-print()
-print("Red wine min values: \n", red_wine_min)
-print()
-print("Red wine max values: \n", red_wine_max)
-print()
-
-white_wine_min, white_wine_max, red_wine_min, red_wine_max
-
+print("Red wine generated samples with adjusted quality: \n", red_wine_samples)
