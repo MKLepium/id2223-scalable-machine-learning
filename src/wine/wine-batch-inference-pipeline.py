941ed36ed8e7988ecf9b41eaf66ed6d9a1269626
MKLepium <maximilian.kurzawski@gmail.com>
2023-11-18 22:28:07 +0100

    added the current implementation of batch-inference-pipeline

--- src/wine/wine-batch-inference-pipeline.py
+++ src/wine/wine-batch-inference-pipeline.py
@@ -16,12 +16,14 @@ def g():
     import joblib
     import datetime
     from PIL import Image
-    from datetime import datetime
+    from datetime import datetime, timedelta
     import dataframe_image as dfi
     from sklearn.metrics import confusion_matrix
     from matplotlib import pyplot
     import seaborn as sns
     import requests
+    import sys
+    import pyarrow as pa
 
     project = hopsworks.login()
     fs = project.get_feature_store()
@@ -30,72 +32,117 @@ def g():
     model = mr.get_model("wine_model", version=1)
     model_dir = model.download()
     model = joblib.load(model_dir + "/wine_model.pkl")
+
+    fg = fs.get_feature_group("wine", version=1)
     
-    feature_view = fs.get_feature_view(name="wine", version=1)
+    feature_view = fs.get_feature_view("wine", version=1)
+    
     batch_data = feature_view.get_batch_data()
+    
+    quality = fg.read()["quality"]
 
-    # batch data split in quality and the rest
-    quality = batch_data['quality']
-    batch_data_to_predict = batch_data.drop(columns=['quality'])
 
     
-    y_pred = model.predict(batch_data_to_predict)
+
+
+    y_pred = model.predict(batch_data)
+
 
     dataset_api = project.get_dataset_api()    
 
-    wine_fg = fs.get_feature_group(name="wine", version=1)
-    df = wine_fg.read() 
+    y_pred_converted = [yp.as_py() if isinstance(yp, pa.lib.Int32Scalar) else yp for yp in y_pred]
+    quality_converted = [ql.as_py() if isinstance(ql, pa.lib.Int32Scalar) else ql for ql in quality]
     
     monitor_fg = fs.get_or_create_feature_group(name="wine_predictions",
                                                 version=1,
-                                                primary_key=["datetime"],
+                                                primary_key=['datetime'],
                                                 description="Wine flower Prediction/Outcome Monitoring"
                                                 )
     
-    now = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-    #data = {
-    #    'prediction': [flower],
-    #    'label': [label],
-    #    'datetime': [now],
-    #   }
-    data = {
-        'prediction': y_pred,
-        'label': quality,
-        'datetime': now,
-       }
-    monitor_df = pd.DataFrame(data)
-    monitor_fg.insert(monitor_df, write_options={"wait_for_job" : False})
     
+    
+    now = datetime.now()
+    data = {
+        'prediction': [y_pred_converted],
+        'label': [quality_converted],
+        'datetime': [now],
+       }
+
+    # Now:
+    """
+    prediction                                         label                                             datetime
+    0  [6, 6, 8, 8, 6, 7, 8, 6, 8, 8, 8, 6, 6, 8, 6, ...  [6, 6, 8, 8, 6, 7, 8, 6, 8, 8, 8, 6, 6, 8, 6, ... 2023-11-18 21:21:23   
+    """
+    # But I want:
+    """
+        prediction  label   datetime
+    0   6           6       2023-11-18 21:21:23
+    1   6           6       2023-11-18 21:21:24
+    ...
+    """
+    data = {
+        'prediction': [y_pred_converted],
+        'label': [quality_converted],
+        'datetime': [now],
+    }
+    #print(data_list)
+    monitor_df = pd.DataFrame(data)
+    # Because I have datetime as a unique, I'll increment it for each item 
+
+
+    monitor_df = convert_data(monitor_df)
+    
+
     history_df = monitor_fg.read()
     # Add our prediction to the history, as the history_df won't have it - 
     # the insertion was done asynchronously, so it will take ~1 min to land on App
     history_df = pd.concat([history_df, monitor_df])
-    monitor_fg.insert(monitor_df, write_options={"wait_for_job" : False})
+    #print(len(history_df))
+    print(history_df.head())
+    monitor_fg.insert(monitor_df)
 
 
     # Create a confusion matrix
     predictions = history_df[['prediction']]
     labels = history_df[['label']]
-
-    print("Number of different wine predictions to date: " + str(predictions.value_counts().count()))
-    print("Number of different wine labels to date: " + str(labels.value_counts().count()))
-
-    # Create a confusion matrix
     cm = confusion_matrix(labels, predictions)
-    #print(cm)
     # save to file
     df_cm = pd.DataFrame(cm)
+    print(df_cm.head())
     fig = pyplot.figure(figsize=(10,7))
     sns.heatmap(df_cm, annot=True, fmt='g')
     pyplot.xlabel("Predicted")
     pyplot.ylabel("Actual")
     pyplot.title("Wine Quality Confusion Matrix")
-    pyplot.savefig("confusion_matrix.png")
+    pyplot.savefig("confusion_matrix_wine.png")
     pyplot.close(fig)
     # upload to hopsworks
-    dataset_api.upload("confusion_matrix.png", "Resources/confusion_matrix.png")
+    # file 
+    #dataset_api.upload("confusion_matrix_wine.png", "Resources/confusion_matrix_wine+.png", overwrite=True)
+    now = now.strftime('%Y-%m-%d_%H_%M_%S')
+    dataset_api.upload("confusion_matrix_wine.png", f"Resources/confusion_matrix_wine_{now}.png", overwrite=True)
 
 
+def convert_data(data):
+    # Unpack the input data
+    import pandas as pd
+    prediction = data['prediction']
+    label = data['label']
+    datetime = data['datetime']
+    from datetime import datetime, timedelta
+
+    # Create a list of dictionaries where each dictionary represents one entry
+    now = datetime.now()
+    data_list = [{'prediction': p, 'label': l, 
+                  'datetime': ((now + timedelta(seconds=i))
+                               .strftime('%Y-%m-%d %H:%M:%S'))
+                               } 
+                 for i, (p, l) in enumerate(zip(prediction[0], label[0]))]
+
+    # Create a DataFrame from the list of dictionaries
+    df = pd.DataFrame(data_list)
+
+    return df
 
 if __name__ == "__main__":
     if LOCAL == True :
